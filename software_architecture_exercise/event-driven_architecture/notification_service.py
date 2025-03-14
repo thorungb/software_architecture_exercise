76f9a37d0503f81de62608ac67e24f5019c6dda2
Thorung Boonkaew <112929023+thorungb@users.noreply.github.com>
2025-03-14 10:29:51 +0700

    Delete software_architecture_exercise directory

--- software_architecture_exercise/event-driven_architecture/notification_service.py
+++
@@ -1,9 +0,0 @@
-class NotificationService:
-    def __init__(self, event_bus):
-        self.event_bus = event_bus
-        self.event_bus.subscribe('order_created', self.handle_order_created)
-
-    def handle_order_created(self, order_data):
-        order_id = order_data['order_id']
-        product_name = order_data['product_name']
-        print(f"Sending notification for Order ID: {order_id} - {product_name}")
